395154a392d91ad379c77d0b565b532ac3b14dce
Aetheridon <lewisvr@protonmail.com>
2023-10-04 21:16:27 +0100

    Added functionality to temporary frontend

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # All current html files are just a temporary front-end interface, after completing the backend then the React front-end development will start.
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        print(username)
     return render_template("login.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        print(f"Username: {username}\nPassword: {password}")
     return render_template("signup.html")
 
 @app.route("/home")
